7e1898e36167600abe191ddb050f3b4294772709
suryapratamatanzil <suryapratamatanzil@mhs.mdp.ac.id>
2023-03-08 09:39:54 +0700

    Suryapratamatanzil/2125250036

--- app/src/main/java/com/if4b/footballplayer/MyDatabaseHelper.java
+++ app/src/main/java/com/if4b/footballplayer/MyDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.if4b.footballplayer;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -28,10 +29,25 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
                 FIELD_NAMA + " VARCHAR(50), " +
                 FIELD_NOMOR + " VARCHAR(2), " +
                 FIELD_KLUB + " VARCHAR(50)" + ")";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS "+ TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
+    public long tambahPlayer(String nama, String nomor, String klub)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
 
+        cv.put(FIELD_NAMA, nama);
+        cv.put(FIELD_NOMOR, nomor);
+        cv.put(FIELD_KLUB, klub);
+
+        long eksekusi = db.insert(TABLE_NAME, null, cv);
+        return eksekusi;
     }
 }
--- app/src/main/java/com/if4b/footballplayer/TambahActivity.java
+++ app/src/main/java/com/if4b/footballplayer/TambahActivity.java
@@ -3,12 +3,55 @@ package com.if4b.footballplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TambahActivity extends AppCompatActivity {
-
+    private EditText etNama, etNomor, etKlub;
+    private Button btnTambah;
+    MyDatabaseHelper myDB = new MyDatabaseHelper(TambahActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etNama = findViewById(R.id.et_nama);
+        etNomor = findViewById(R.id.et_nomor);
+        etKlub = findViewById(R.id.et_club);
+        btnTambah = findViewById(R.id.btn_simpan);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama , nomor , klub;
+
+                nama = etNama.getText().toString();
+                nomor = etNomor.getText().toString();
+                klub = etKlub.getText().toString();
+
+                if(nama.trim().equals("")){
+                    etNama.setError("Please Insert Your Name!");
+                }
+                if(nomor.trim().equals("")){
+                    etNomor.setError("Please Insert Your Jersey Number!");
+                }
+                if(klub.trim().equals("")){
+                    etKlub.setError("Please Insert Your Name!");
+                }
+                else {
+                    long eks = myDB.tambahPlayer(nama, nomor, klub);
+                    if(eks ==-1){
+                        Toast.makeText(TambahActivity.this, "Fail Adding Data", Toast.LENGTH_SHORT).show();
+                        etNama.requestFocus();
+                    }
+                    else{
+                        Toast.makeText(TambahActivity.this, "Success Adding Data", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            }
+        });
     }
 }
